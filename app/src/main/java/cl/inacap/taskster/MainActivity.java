12dead21d88704059d1c1ae6f5890350a554a19b
GastonUC <Gacox.magallanes@gmail.com>
2020-10-26 00:26:54 -0300

    Svg Icons agregados, Intent a NuevaNota

--- app/src/main/java/cl/inacap/taskster/MainActivity.java
+++ app/src/main/java/cl/inacap/taskster/MainActivity.java
@@ -2,7 +2,9 @@ package cl.inacap.taskster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AgregarNota(View view) {
+        Intent intent=new Intent(this,NuevaNotaActivity.class);
+        startActivity(intent);
     }
 }
