72630ef733282e06e863cf8ba5cd5cb480ed0665
GastonUC <Gacox.magallanes@gmail.com>
2020-10-25 23:33:21 -0300

    Nuevo Activity, dependency, string y color

---
+++ app/src/main/java/cl/inacap/taskster/NuevaNotaActivity.java
@@ -0,0 +1,14 @@
+package cl.inacap.taskster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_nota);
+    }
+}
---
+++ app/src/main/java/cl/inacap/taskster/modelo/Nota.java
@@ -0,0 +1,47 @@
+package cl.inacap.taskster.modelo;
+
+public class Nota {
+    private String titulo;
+    private String descripcion;
+    private Boolean estado;
+
+    public static final Nota[] notas={
+            new Nota("Nota de prueba", "Algo para escribir en esta nota...", true)
+    };
+
+    public Nota(String titulo, String descripcion, Boolean estado) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + '\'' +
+                descripcion;
+    }
+}
