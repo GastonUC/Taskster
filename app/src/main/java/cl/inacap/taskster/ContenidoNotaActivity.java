32ee1a5d7f90e8cc0ba138ddaf823ae2478b3772
GastonUC <Gacox.magallanes@gmail.com>
2020-10-29 15:50:48 -0300

    Se añade función para ver contenido de las notas

--- app/src/main/java/cl/inacap/taskster/ContenidoNotaActivity.java
+++ app/src/main/java/cl/inacap/taskster/ContenidoNotaActivity.java
@@ -3,12 +3,41 @@ package cl.inacap.taskster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContenidoNotaActivity extends AppCompatActivity {
+
+    TextView title;
+    TextView descripcion;
+
+    String data1;
+    String data2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenido_nota);
+
+        title = findViewById(R.id.title);
+        descripcion = findViewById(R.id.desc);
+
+        getData();
+        setData();
+    }
+
+    private void getData() {
+        if(getIntent().hasExtra("data1") && getIntent().hasExtra("data2")) {
+            data1 = getIntent().getStringExtra("data1");
+            data2 = getIntent().getStringExtra("data2");
+
+        }else{
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setData() {
+        title.setText(data1);
+        descripcion.setText(data2);
     }
 }
--- app/src/main/java/cl/inacap/taskster/modelo/Adaptador.java
+++ app/src/main/java/cl/inacap/taskster/modelo/Adaptador.java
@@ -1,14 +1,17 @@
 package cl.inacap.taskster.modelo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import cl.inacap.taskster.ContenidoNotaActivity;
 import cl.inacap.taskster.R;
 
 public class Adaptador extends RecyclerView.Adapter<Adaptador.MyViewHolder> {
@@ -32,9 +35,19 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
         holder.text1.setText(data1[position]);
         holder.text2.setText(data2[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ContenidoNotaActivity.class);
+                intent.putExtra("data1", data1[position]);
+                intent.putExtra("data2", data2[position]);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -45,11 +58,13 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView text1, text2;
+        ConstraintLayout mainLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             text1 = itemView.findViewById(R.id.tit_tarjeta);
             text2 = itemView.findViewById(R.id.conten_tarjeta);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
         }
     }
 }
